6569e5735714cee83e06d93e65c004bbd51c8d9b
usk79 <skies_of_blue@big-tree-leo.sakura.ne.jp>
2018-04-01 02:46:24 +0000

    adjust new action for points

--- app/models/point.rb
+++ app/models/point.rb
@@ -5,17 +5,16 @@ class Point < ApplicationRecord
   validates :lng, numericality: true
   
   def self.to_json_for_gmap(arg) # 各ポイント情報が入ったjsonを作成
-    ary = []
-    
     if arg.class == ActiveRecord_Relation
+      data = []
       arg.each do |point|
-        ary.push({description:point.name, pos:{lat:point.lat, lng:point.lng}})
+        data.push({description:point.name, pos:{lat:point.lat, lng:point.lng}})
       end
     elsif arg.class == Point
-      ary.push({description: nil, pos:{lat: arg.lat, lng: arg.lng}})
+      data = {description: nil, pos:{lat: arg.lat, lng: arg.lng}}
     end
       
-    ary.to_json
+    data.to_json
   end
-
+  
 end
