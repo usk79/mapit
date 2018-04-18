331a8963a3e627dadafd1855accdf463c2fcc73e
usk79 <skies_of_blue@big-tree-leo.sakura.ne.jp>
2018-04-18 13:31:09 +0000

    toppage collection

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,11 +1,18 @@
 class ToppagesController < ApplicationController
+    before_action :login_check
+    
   def index
-    if logged_in?
-        redirect_to controller: 'collections', action: 'index'
-    else
-      @public_collections = public_user.collections
-    end
+    @public_collections = public_user.collections
   end
   
-
+  def show
+    @public_collections = public_user.collections
+    @collection = @public_collections.find_by(id: params[:id])
+    @points = @collection.points
+  end
+  
+  private
+  def login_check
+    redirect_to controller: 'collections', action: 'index' if logged_in?
+  end
 end
