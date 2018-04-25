abead6dfaf1057532365400785c38359d82a8827
usk79 <skies_of_blue@big-tree-leo.sakura.ne.jp>
2018-04-25 13:26:00 +0000

    gmapkey

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  before_action :set_gmapkey
   include SessionsHelper
   
   def public_user
@@ -10,7 +10,6 @@ class ApplicationController < ActionController::Base
   def find_from_public_collection(id)
     public_user.collections.find_by(id: id)
   end
-
   
   private
   
@@ -19,4 +18,13 @@ class ApplicationController < ActionController::Base
       redirect_to signin_url
     end
   end
+  
+  def set_gmapkey
+    if Rails.env.production?
+      @gmap_key = 'AIzaSyAqKNbKg15mXDaj3hDLpgIfrIDWYtSMdSQ'
+    else
+      @gmap_key = 'AIzaSyAIJfGcO2u5nHVWO-b7O4pAfPrta3U3EHc'
+    end
+  end
+  
 end
